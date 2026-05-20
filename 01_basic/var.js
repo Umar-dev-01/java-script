c7d82c541eeef8ae97fc70e4a435922c4e96bf48
Umar-dev-01 <01.dev.umar@gmail.com>
2026-05-20 22:14:10 +0500

    str 2 num convr

---
+++ 01_basic/var.js
@@ -0,0 +1,48 @@
+/*const studentId = 123
+var studentName = "Malik"
+let gmail = "abc@gmail.com"
+cityName = "Islamabad"
+
+studentName= "Awan"
+gmail= "umSha@gmail.com"
+console.log(studentName)
+console.log([studentId, gmail, cityName])*/
+
+/*let gmail= "umSha@gmail.com"
+var name ="Umar"
+console.log("My name is "+name +" and my gmail is "+ gmail)*/
+
+
+// Data Types
+
+/*console.log(5-2);
+console.log("Malik");*/
+
+/*let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);*/
+
+/*var marks = "90";
+
+let mainNumber = Number(marks);
+
+console.log(mainNumber);
+console.log(typeof mainNumber);*/
+
+var price = 1500
+let Value= String (price);
+console.log(Value);
+console.log(typeof Value);
+
+
+ console.log("100" + 20 - 10);
+console.log("100" - 20 + 10);
+console.log(true + "5");
+output:
+10010
+90
+true
+ 
+
