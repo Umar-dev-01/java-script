17f449d5089a1a766d8bf20940965b266c0febad
Umar-dev-01 <01.dev.umar@gmail.com>
2026-05-21 10:40:42 +0500

    betu

--- 01_basic/var.js
+++ 01_basic/var.js
@@ -31,7 +31,7 @@ let mainNumber = Number(marks);
 console.log(mainNumber);
 console.log(typeof mainNumber);*/
 
-var price = 1500
+/*var price = 1500
 let Value= String (price);
 console.log(Value);
 console.log(typeof Value);
@@ -44,5 +44,99 @@ output:
 10010
 90
 true
- 
 
+console.log("20" + 5);
+console.log("20" - 5);
+console.log("20" * 2);
+console.log("20" / 2);
+console.log(Number("100"));
+205 String
+15 Number
+40 Number
+10 Number*/
+
+/*console.log("5" + 2 + 3);
+console.log("5" - 2 + 3);
+55 Number
+6 Number
+ye ap ny likhy wi ghlt hen esy likhty hn console.log(("5" + 2) + 3);
+console.log(("5" - 2 )+ 3);
+
+console.log(Number("50"));
+console.log(Number("50abc"));
+console.log(Number(true));
+console.log(Number(false));
+50 Number
+50abc Number
+1 Number
+0 Number
+
+let value = "100";
+
+value = Number(value);
+
+value = value + 50;
+
+console.log(value);
+console.log(typeof value);
+"150" Num
+agr Number() remove kr den to datatype jo chane ho rahi hy wo nahen ho pay gi and program run bhi nahen kry ga 
+
+
+console.log(Boolean(""));
+console.log(Boolean(" "));
+console.log(Boolean(0));
+console.log(Boolean(1));
+1
+0
+false
+true*/
+
+
+/*console.log(10 + "5" + 2);
+console.log(10 - "5" + 2);
+17 Number becz first num hy tbi sb add ho rhy agr string hota to string bn jaty agy waly 
+7 and same wohi logic 
+
+Q2 nahen ata 
+let score = "25";
+
+score = score + 5;
+score = Number(score);
+
+console.log(score);
+console.log(typeof score);
+phly let kia score and osy value di "25" jo k string hy and phir score jo string declear tha os men num +5 kr diy giy 
+wo bn giy "255" and phir on ki datatype Num men convert kr di gaie score jo k phly string tha phir num bn gia and 
+phir osy print krwa dia gia
+255
+Number
+
+console.log(Number(""));
+console.log(Number(" "));
+console.log(Number(null));
+console.log(Number(undefined));
+false
+true
+0
+undefined*/
+
+console.log("10" + 5 - 2 + "3");
+"133" String
+
+console.log(true + false + "1");
+console.log("1" + true + false);
+"01" String
+NaN
+
+console.log("100" - "50" + "20");
+console.log("100" + "50" - "20");
+"5020"
+10030 Num 
+
+console.log([] + []);
+console.log([] + {});
+console.log({} + []);
+2 Num
+0 Num
+1
