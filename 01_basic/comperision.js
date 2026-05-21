204a10e71e9f25d24d17a42fb5a6c9656476268d
Umar-dev-01 <01.dev.umar@gmail.com>
2026-05-21 19:51:31 +0500

    comp

---
+++ 01_basic/comperision.js
@@ -0,0 +1,156 @@
+/*console.log(0 == false);
+console.log(0 === false);
+console.log("" == 0);
+console.log("" === 0);
+true
+true
+false
+true
+
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(typeof null);
+console.log(typeof undefined);
+false
+true
+NaN
+Object
+
+
+console.log(NaN == NaN);
+console.log(NaN === NaN);
+console.log(typeof NaN);
+true
+true
+undefined
+
+console.log(" " == 0);
+console.log(" " === 0);
+console.log(false == "0");
+console.log(false === "0");
+false
+false
+true
+true
+
+console.log([] == 0);
+console.log([] === 0);
+console.log([1] == 1);
+console.log([1] === 1); ye nae ata*/
+
+/*console.log(5 == "5");
+console.log(5 === "5");
+true bex == only value
+false === check only datatype
+
+console.log(false == 0);
+console.log(false === 0);
+ek true hy == value same
+data type true same hy
+
+console.log(null == undefined);
+console.log(null === undefined);
+wohi operater ek double and ek triple 
+
+console.log("" == 0);
+console.log("" === 0);
+console.log("0" == false);
+true
+true
+true*/
+
+/*console.log(10 == "10");
+console.log(10 === "10");
+console.log(typeof "10");
+true
+false
+Number
+
+console.log(true == 1);
+console.log(false == "");
+console.log(false === "");
+true 
+true 
+false
+
+
+let value = "20";
+
+console.log(value + 5);
+console.log(value - 5);
+
+value = Number(value);
+
+console.log(value + 5);
+
+"205"
+15
+25*/
+
+/*console.log("2" + 2 + 2);
+console.log(2 + 2 + "2");
+console.log("2" - 2 + 2);
+"222"
+"42"
+2
+
+console.log(true + "1");
+console.log(false + 1);
+console.log(true + false + "1");
+"2"
+1
+"21"
+
+console.log("10" - "4" + "2");
+console.log("10" + "4" - "2");
+console.log("10" - "abc");
+"62"
+12
+NaN
+
+console.log([] + []);
+console.log([] + {});
+console.log({} + []);
+null
+null
+null*/
+
+console.log("5" + 1 + 2);
+console.log(5 + 1 + "2");
+console.log("5" - 1 + 2);
+"512"
+"62"
+6
+
+console.log(true + "0");
+console.log(false + false + "1");
+console.log("10" - "5" + "1");
+"true0"
+"1"
+"51"
+
+console.log([] + 1);
+console.log([] + "1");
+console.log({} + 1);
+console.log({} + "1");
+1
+"1"
+Object
+NaN
+
+console.log([] == "");
+console.log([] === "");
+console.log({} == "[object Object]");
+console.log({} === "[object Object]");
+true
+false
+true
+
+console.log(0 != false);
+console.log(0 !== false);
+console.log("" != 0);
+console.log("" !== 0);
+true
+true
+false
+true
