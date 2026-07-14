0ec418fed3d5d4cb0e2981b2c2b0fdcff016ef62
Umar-dev-01 <01.dev.umar@gmail.com>
2026-07-14 08:28:46 +0500

    kekerr

---
+++ 01_basic/stack&heap.js
@@ -0,0 +1,66 @@
+/*let a = 50;
+let b = a;
+
+b = 100;
+console.log(a);
+console.log(b);
+50
+100
+
+let user1 = {
+  name: "Umar"
+};
+
+let user2 = user1;
+
+user2.name = "Ali";
+
+console.log(user1.name);
+console.log(user2.name);
+ali
+ali
+
+let arr1 = [1, 2, 3];
+
+let arr2 = arr1;
+
+arr2[0] = 100;
+
+console.log(arr1);
+console.log(arr2);
+100
+100*/
+
+let x = 10;
+
+let y = x;
+
+y = 50;
+
+console.log(x);
+console.log(y);
+10
+50
+
+let car1 = {
+  brand: "BMW"
+};
+
+let car2 = car1;
+
+car2.brand = "Audi";
+
+console.log(car1.brand);
+console.log(car2.brand);
+"Audi"
+"Audi"
+
+let numbers1 = [10, 20];
+
+let numbers2 = numbers1;
+
+numbers2[1] = 500;
+
+console.log(numbers1);
+console.log(numbers2);
+
