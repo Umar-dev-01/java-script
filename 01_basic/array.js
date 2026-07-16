2c98e9e2f34f1d7151f4dcbee67062c9366d4c03
Umar-dev-01 <01.dev.umar@gmail.com>
2026-07-16 16:00:16 +0500

    uni

---
+++ 01_basic/array.js
@@ -0,0 +1,87 @@
+//let arr = ["Ali", "Ahmed", "Umar"]
+// first question saie sy nae ata
+/*let fruits = ["pinApple", "Bnana", "Mango"]
+fruits.push("Orange")
+console.log(fruits);
+fruits.pop()
+console.log(fruits);
+
+let fruits = ["pinApple", "Bnana", "Mango"]
+console.log(fruits.includes("Bnana"))
+console.log(fruits.includes("cake"))
+
+let fruits = ["pinApple", "Bnana", "Mango"]
+console.log(fruits.indexOf("Mango"))
+
+
+let arr = [10,20,30,40,50];
+console.log(arr.slice(1,4))
+console.log(arr.splice(1,3))
+
+
+
+//Ali, Ahmed, Umar, Hamza
+let names = ["Ali", "Ahmed", "Umar", "Hamza"]
+console.log(names[0])
+console.log(names[3])
+//who is the best
+console.log(names.splice(1,2))
+console.log(names)
+
+let fruits = ["Apple", "Banana"];
+fruits.push("Mango")
+fruits.unshift("Orange")
+console.log(fruits)
+
+let numbers = [10,20,30,40,50];
+numbers.pop()
+numbers.shift()
+console.log(numbers)
+
+
+let cities = ["Lahore","Karachi","Islamabad","Peshawar"];
+console.log(cities.indexOf("Islamabad"))
+console.log(cities.findIndex("Quetta"))
+
+let arr = [5,10,15,20,25,30];
+console.log(arr.slice(2,5))
+console.log(arr)
+
+let arr = [5,10,15,20,25,30];
+arr.splice(10,15,20)
+console.log(arr)
+
+let colors = ["Red","Blue","Green"];
+colors.join()
+console.log(colors)
+
+let marks = [80,90,70];
+marks.push(100)
+marks.unshift(60)
+console.log(marks)
+
+
+let arr = ["A","B","C"];
+
+arr.push("D");
+arr.shift();
+arr.unshift("Z");
+arr.pop();
+
+console.log(arr);
+out is "Z","B","C"
+*/
+
+
+let arr = ["Laptop", "Mouse", "Keyboard", "Monitor"]
+
+console.log(arr[1])
+//console.log(arr.find("Monitor"))
+
+console.log(arr.indexOf("Keyboard"))
+arr.push("Speaker")
+console.log(arr)
+arr.shift()
+console.log(arr)
+
+
