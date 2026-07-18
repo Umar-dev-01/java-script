600710470a7d2c6e433c29b8639617bf66bd0003
Umar-dev-01 <01.dev.umar@gmail.com>
2026-07-18 09:25:08 +0500

    arry

--- 01_basic/array.js
+++ 01_basic/array.js
@@ -70,7 +70,7 @@ arr.pop();
 
 console.log(arr);
 out is "Z","B","C"
-*/
+
 
 
 let arr = ["Laptop", "Mouse", "Keyboard", "Monitor"]
@@ -85,3 +85,79 @@ arr.shift()
 console.log(arr)
 
 
+let fruits = ["Apple", "Banana", "Mango"];
+//Apple | Banana | Mango
+console.log(fruits.join("|"))
+
+let arr = [1,2,3,4,5,6];
+//[2,3,4]
+
+console.log(arr.splice(1,3))
+
+/*
+let arr = [1,2,3,4,5,6];
+//2,3,4
+console.log(arr.splice(1,3))
+console.log(arr)
+
+
+// slice and splice ki abhi etni smjh nhen hy splice phir bhi koch na koch smjh a gia hy slice na aia 
+
+let fruits = ["Apple","Banana","Mango"]
+
+console.log(fruits.join("-"))
+
+console.log(fruits.include("Apple"))
+
+console.log(fruits.splice(2,4))
+
+console.log(fruits.length())
+
+
+
+console.log(arr.indexOf("ALi"))
+// jo jo ziada importnt hoty hn on py ziada dhian dia kro
+
+
+//let arr = [10,20,30,40,50];
+//20
+//30
+//40
+
+//console.log(arr.slice(1,4))
+//console.log(arr)
+console.log(arr.splice(1,3))
+console.log(arr)
+
+//hint na dia kro ab to ap hint dyd ety ho mn kr leta hon jb bad men jata hon to bhol jata hon k kis kam k liy konsa function ya 
+//ya kia use krna hota hy
+//arry to 0 sy start hota hy lk slice and splice 1 sy keon 
+
+//mojhy abhi function and property ki koe smjh nhen hy k kesy phchan ki jati hy and keon use kiy jaty hn 
+
+
+//Q1. arry koe bhi bhot si chezon ko akhta ek block mn likhny k liy use hota hota hy
+//Q2. keon k arry ka rule hy k indexing 0 sy start ki jay(ans nae pta eska apny sy chora)
+
+//Q1. 
+// let arr = ["HTML", "CSS", "JavaScript"];
+console.log(arr.length)
+console.log(arr[2])
+Q2
+let nums = [1,2,3];
+nums.push(4)
+nums.unshift(0)
+console.log(nums)
+Q3*/
+let fruits = ["Apple","Banana","Mango"];
+console.log(fruits.includes("Banana"))
+console.log(fruits.indexOf("Mango"))
+/*
+Q4
+let arr = [5,10,15,20,25];
+//10,15,20
+console.log(arr.slice(1,4))
+console.log(arr)
+
+console.log(arr.splice(1,3))
+console.log(arr)*/
