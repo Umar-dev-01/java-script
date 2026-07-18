70564959df83e7e236a7824c2ba7f0d05384d2c1
Umar-dev-01 <01.dev.umar@gmail.com>
2026-07-18 21:44:32 +0500

    array-ful

--- 01_basic/array.js
+++ 01_basic/array.js
@@ -148,7 +148,7 @@ let nums = [1,2,3];
 nums.push(4)
 nums.unshift(0)
 console.log(nums)
-Q3*/
+Q3
 let fruits = ["Apple","Banana","Mango"];
 console.log(fruits.includes("Banana"))
 console.log(fruits.indexOf("Mango"))
@@ -160,4 +160,212 @@ console.log(arr.slice(1,4))
 console.log(arr)
 
 console.log(arr.splice(1,3))
-console.log(arr)*/
+console.log(arr)
+
+let marvel = ["Ironman", "Thor", "Hulk"];
+let dc = ["Batman", "Superman"];
+let marvel= dc()
+consol.log(marvel.concat(dc))
+
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+
+arr1.push(arr2);
+
+console.log(arr1);
+//[1,2,[3,4]]
+
+let arr1 = [1, 2];
+let arr2 = [3, 4];
+
+let arr3 = arr1.concat(arr2);
+
+console.log(arr3);
+console.log(arr1);
+//[1,2,3,4]
+//[1,2]
+
+let arr = [1, 2, [3, 4], [5, [6, 7]]];
+console.log(arr.flat())
+//true
+//false
+//false
+
+console.log(Array.from("Umar"));
+//"u","m","a","r"
+
+
+
+let arr1 = [100]
+let arr2 = [200]
+let arr3 =[300]
+
+let marvel = ["Ironman", "Thor"];
+let dc = ["Batman", "Superman"];
+
+let heroes = marvel.concat(dc);
+
+console.log(Array.isArray(heroes));
+
+console.log(Array.from("JS"));
+
+console.log(Array.of(1,2,3));
+
+//Agar mere paas 3 arrays hain aur mujhe unhe ek array me merge karna hai, to tum kaun sa method prefer karoge aur kyun?
+//ans is (...)
+
+let fruits = ["Apple", "Banana"];
+let vegetables = ["Potato", "Tomato"];
+let food = fruits.vegetables
+console.log(...fruits,...vegetables)
+console.log(food)
+
+
+let a = [1,2];
+let b = [3,4];
+let c = [5,6];
+console.log(...a,...b,...c)
+
+let a = [10,20];
+let b = [30,40];
+a.push(b);
+
+console.log(a.length);
+console.log(a[2]);
+//4
+//30
+
+let a = [10,20];
+let b = [30,40];
+let c = [...a,...b];
+
+console.log(c.length);
+console.log(c[3]);
+//3
+//40
+
+
+let arr = [1,[2,[3,[4,[5]]]]];
+console.log(???(arr))
+
+
+console.log(Array.isArray([]));
+console.log(Array.isArray({}));
+console.log(Array.isArray("[]"));
+console.log(Array.isArray(123));
+//false
+//false
+//false
+//true
+
+
+console.log(Array.from("Pakistan"));
+"P","a","k","i","s","t","a","n"
+
+let x = 50;
+let y = 100;
+let z = 150;
+console.log((Array.of(x,y,z).length))
+
+let a = ["A","B"];
+let b = ["C","D"];
+
+let c = a.concat(b);
+
+console.log(Array.isArray(c));
+
+console.log(Array.from("ABC"));
+
+console.log(c.length);
+
+console.log(Array.of(1,2));
+
+//concat() aur spread (...) me se tum kis ko prefer karoge aur kyun?
+//ans(concat ko keon k es men arrys k sath single value bhi alg sy add ho skti hy)
+//Array.from() aur Array.of() me kya difference hai?
+//ans i dont know
+?
+
+
+
+let a = [1,2];
+let b = [3,4];
+
+console.log(a.push(b));
+console.log(a);
+//1,2,3,4
+//1,2
+/*
+let a = [1,2];
+let b = [3,4];
+
+let c = [...a,...b];
+
+console.log(c);
+console.log(c.length);
+//1,2,3,4
+//3
+//mn smjha counting 0 sy strat hoti hy and agr mn  ny ghlti ki ho to btaia kro k ye esy saie ho ga
+
+console.log(Array.isArray([]));
+console.log(Array.from("JS"));
+console.log(Array.of(10,20,30));
+true
+true 
+true*/
+
+//true
+
+  //Q2. false Q3. [" A", "I"] Q4. [100,200,300 ]
+  //  Q5. Array.isArray()  pochta hy dekhta hy k ye arry hy k nae Array.from()  
+  //  ye strings ko arry bnata hy Array.of()  numbers ko arry bnata hy
+
+  /*let frontend = ["HTML", "CSS", "JavaScript"];
+let backend = ["Node.js", "Express"];
+frontend.push(backend)
+console.log(frontend)
+
+//lkn ye nhen likha k arrays k andr arrays ho ya na ho wrna ... use kr lety
+let name = "Umar";
+console.log(Array.from(name))
+
+let a = 10;
+let b = 20;
+let c = 30;
+let d= [...10,...20,...30]
+console.log(d)
+*/
+//let data = {name: "Umar"};
+//true
+/*
+let arr1 = [1,2];
+let arr2 = [3,4];
+
+let result = [...arr1, 100, ...arr2];
+
+console.log(result);
+//[1,2,3,100,3,4]
+
+
+let arr = [1,[2,[3,[4]]]];
+console.log(arr.flat(Infinity));
+//[1,2,3,4]
+
+
+let mobiles = ["iPhone", "Samsung"];
+let laptops = ["Dell", "HP"];
+let watches = ["Apple Watch", "Galaxy Watch"];
+let products = [...mobiles,...laptops,...watches]
+console.log(products)
+
+
+let text = "AI";
+
+console.log(Array.from(text));
+
+console.log(Array.isArray([]));
+
+console.log(Array.of(5,10));
+
+*/
+
